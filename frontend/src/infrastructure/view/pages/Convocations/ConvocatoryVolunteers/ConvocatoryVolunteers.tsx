7c78f5be798ffd5ec56417e509b43485a5dcd06c
Javier Díaz Domínguez <javidiazdom@gmail.com>
2021-01-22 10:22:23 +0000

    :sparkles: Convocatory volunteers viewç

--- frontend/src/infrastructure/view/pages/Convocations/ConvocatoryVolunteers/ConvocatoryVolunteers.tsx
+++ frontend/src/infrastructure/view/pages/Convocations/ConvocatoryVolunteers/ConvocatoryVolunteers.tsx
@@ -1,24 +1,43 @@
-import { array } from '@storybook/addon-knobs';
 import * as React from 'react';
 import './ConvocatoryVolunteers.scss';
 import { Volunteer } from '../../../../../domain/models/Volunteer';
 import volunteers from './VolunteersMock.json';
+import { ReactComponent as ProfileSVG } from '../assets/Convocatory-icons/assignment_ind.svg';
 
 export const ConvocatoryVolunteers: React.FC<{}> = () => {
   var convocatoryVolunteers = volunteers as Volunteer[];
+  var dropedVolunteers = volunteers as Volunteer[];
   return (
     <div className="ConvocatoryVolunteers">
-      <h1>Voluntarios para {"Nombre del voluntariado"}</h1>
-      <div className="Volunteers">
+      <h1>Voluntarios inscritos a {"Nombre del voluntariado"}</h1>
+      <div className="volunteers">
         {
           convocatoryVolunteers.map((volunteer: Volunteer) => {
-            return (<div className="Volunteer">
+            return (<div className="volunteer">
               <p>{volunteer.name} {volunteer.surname}</p>
+              <div className="buttons">
+                <ProfileSVG />
+                <div className="checkbox" />
+              </div>
             </div>);
           })
         }
       </div>
-    </div>
+      <div className="volunteers dropped">
+        <h1>Voluntarios desestimados</h1>
+        {
+          dropedVolunteers.map((volunteer: Volunteer) => {
+            return (<div className="volunteer">
+              <p>{volunteer.name} {volunteer.surname}</p>
+              <div className="buttons">
+                <ProfileSVG />
+                <div className="checkbox" />
+              </div>
+            </div>);
+          })
+        }
+      </div>
+    </div >
   );
 }
 
